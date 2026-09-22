9b3f7eb82a13dfb39db60bb3e83e13e1b0d32228
thawn3 <sangn@nsuok.edu>
2026-09-22 17:10:14 -0500

    test

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-// puts the App.jsx on the webpage
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
